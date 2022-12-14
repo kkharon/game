80e6be5e45bd2341e9563c589f4a72f742f43025
kkharon <83268917+kkharon@users.noreply.github.com>
2022-12-14 23:19:28 +0300

    updated

--- 123.c
+++ 123.c
@@ -3,56 +3,16 @@
 #include <string.h>
 #include <time.h>
 #include <math.h>
+#include "functions.h"
+#include "utils.h"
 
 #define STATE_STARTING      1
 
-void print( const char *words )
-{
-    printf( "%s\n", words );
-}
-
-int getNumericInput()
-{
-    int  result = -1;
-    char buffer1[64];   /* somewhere to store user input */
-    char *endstr;
-
-    while ( result == -1 )
-    {         
-        printf("    ? ");
-        scanf("%s", buffer1);
-             /* read from the console into the buffer */
-        result = (int)strtol( buffer1, &endstr, 10 );     /* only simple error checking */
-
-        if ( endstr == buffer1 )                          /* was the string -> integer ok? */
-            result = -1;
-    }
-
-    return result;
-}
-char getYesOrNo()
-{
-    printf("TRY AGAIN? ");
-    char result1 = '!';
-    char buffer1[64];   
-    do
-    {
-        
-        scanf("%s", buffer1);
-        // fgets( buffer1, sizeof( buffer1 ), stdin );            
-        if ( buffer1[0] == 'Y' || buffer1[0] == 'y' )
-            result1 = 'Y';
-        else if ( buffer1[0] == 'N' || buffer1[0] == 'n' )
-            result1 = 'N';
-    } while ( !( result1 == 'Y' || result1 == 'N' ) );
-
-    return result1;
-}
 void showIntroduction()
 {
-  printf("                     CLOSE ENCOUNTERS\n" );
-  printf("                    CREATIVE COMPUTING\n" );
-  printf("                  MORRISTOWN, NEW JERSEY\n\n\n\n" );
+  PRINT("                     CLOSE ENCOUNTERS\n" );
+  PRINT("                    CREATIVE COMPUTING\n" );
+  PRINT("                  MORRISTOWN, NEW JERSEY\n\n\n\n" );
 }
 
 float randomAB(float a, float b)
@@ -70,8 +30,8 @@ float randFloat()
 int main(void) {
   int   game_state = STATE_STARTING;
   srand(time(NULL));
-  char buffer[64]; 
-  char  yes_or_no;
+  // char buffer[64]; 
+  // char  yes_or_no;
   float X;
   float C;
   float H;
@@ -86,6 +46,7 @@ int main(void) {
   int M;
   int N = 0;
   int ABC = 1;
+  char *rules_flagY;
  
 while (1)
   {
@@ -123,25 +84,24 @@ while (1)
           }
           break;
         }
-        print("YOU ARE SITUATED AT COORDINATES (0,0).  A UFO IS");
-        print( "HEADING FOR IMPACT AT THAT LOCATION.\n");
-        printf( "\nAT FIRST TRACK, THE UFO IS ON A COURSE OF %.f DEGREES ",Y);
-        print( "LONGITUDE");
-        printf( "AND %.f DEGREES LATITUDE.\n",Z);
-        printf( "   (ALSO IT'S FALLING AT A SPEED OF %.f MILES/PER HOUR)\n",X);
-        print( "YOU HAVE THE FOLLOWING ALTERNATIVES:");
-        print( "A) YOU CAN ATTEMPT TO SHOOT THE UFO OUT OF THE SKY.  OR");
-        print( "B) YOU CAN DO NOTHING, AND HOPE THAT AIR FRICTION");
-        print( "   WILL CAUSE THE COURSE OF THE UFO TO DEVIATE, AND THUS");
-        print( "   MISS YOUR LOCATION.\n");
+        PRINT("YOU ARE SITUATED AT COORDINATES (0,0).  A UFO IS");
+        PRINT( "HEADING FOR IMPACT AT THAT LOCATION.\n");
+        PRINT( "\nAT FIRST TRACK, THE UFO IS ON A COURSE OF %.f DEGREES ",Y);
+        PRINT( "LONGITUDE\n");
+        PRINT( "AND %.f DEGREES LATITUDE.\n",Z);
+        PRINT( "   (ALSO IT'S FALLING AT A SPEED OF %.f MILES/PER HOUR)\n",X);
+        PRINT( "YOU HAVE THE FOLLOWING ALTERNATIVES:\n");
+        PRINT( "A) YOU CAN ATTEMPT TO SHOOT THE UFO OUT OF THE SKY.  OR\n");
+        PRINT( "B) YOU CAN DO NOTHING, AND HOPE THAT AIR FRICTION\n");
+        PRINT( "   WILL CAUSE THE COURSE OF THE UFO TO DEVIATE, AND THUS\n");
+        PRINT( "   MISS YOUR LOCATION.\n");
         game_state = STATE_CHOOSE;
       } 
       else if (game_state == STATE_CHOOSE) 
       {
         if (N == 0) {
-          printf( "WHAT IS YOUR PLAN OF ACTION (A OR B)? ");
-          scanf("%s", buffer);
-          
+          PRINT( "WHAT IS YOUR PLAN OF ACTION (A OR B)? ");
+          input_string(&rules_flagY);
         } else {
            while (Z <100){
           Z=(randFloat()*140);
@@ -169,40 +129,42 @@ while (1)
           }
           break;
         }
-          printf("PLAN (A OR B)? ");
-          scanf("%s", buffer);
+          PRINT("\nPLAN (A OR B)? ");
+          input_string(&rules_flagY);
         }
-          if ( buffer[0] == 'A' || buffer[0] == 'a' )
+          if ( strcmp (rules_flagY, "A") == 0 )
           {
             T = 0;
             if (ABC > 0) {
-                print("\nLAUNCH AN I.C.B.M. ON A COURSE THAT WILL INTERCEPT THE UFO");
-                print("WITHOUT THE UFO BEING TOO CLOSE TO YOUR LOCATION.  IF THE SPEED");
-                printf("OF THE UFO EXCEEDS %4.f M.P.H., YOUR MISSILES ARE USELESS!\n",H);
+                PRINT("\nLAUNCH AN I.C.B.M. ON A COURSE THAT WILL INTERCEPT THE UFO\n");
+                PRINT("WITHOUT THE UFO BEING TOO CLOSE TO YOUR LOCATION.  IF THE SPEED\n");
+                PRINT("OF THE UFO EXCEEDS %4.f M.P.H., YOUR MISSILES ARE USELESS!\n",H);
                 ABC = ABC - 1;
             }
-            printf("--SCANNERS PREDICT YOU HAVE %d MINUTES TO DESTROY THE UFO\n",M);
-            print("BEFORE IT IS TOO CLOSE TO IMPACT.\n");
+            PRINT("--SCANNERS PREDICT YOU HAVE %d MINUTES TO DESTROY THE UFO\n",M);
+            PRINT("BEFORE IT IS TOO CLOSE TO IMPACT.\n");
             C=Z-Y+I-(randFloat()*4)+1;
             float O = C+F;
-            printf("--COMPUTER INDICATES COURSE AT FIRST SIGHTING IS APPROX. %1.f\n", O); 
-            print("\n\nTYPE IN A COURSE ON THE CHART BELOU\n\n");
-            print("TIME          SPEED          LONGITUDE          LATITUDE          COURSE");
+            PRINT("--COMPUTER INDICATES COURSE AT FIRST SIGHTING IS APPROX. %1.f\n", O); 
+            PRINT("\n\nTYPE IN A COURSE ON THE CHART BELOU\n\n");
+            PRINT("TIME          SPEED          LONGITUDE          LATITUDE          COURSE\n");
             while (T<=5) 
             {
               int count1;
-              printf(" %.1f         ",T);
-              printf(" %.f           ", X);
-              printf(" %.f            ", Y);
-              printf("     %.f          ", Z);
-              count1 = getNumericInput();
+              PRINT(" %.1f         ",T);
+              PRINT(" %.f           ", X);
+              PRINT(" %.f            ", Y);
+              PRINT("     %.f               ", Z);
+              input_number(&count1);
               if (T == M) {
-                print("---------------ATTENTION----------------");
-                printf("ELAPSED-TIME-INDICATES-THAT- %d MINUTES-HAVE-PASSED.--IF-YOU-\n",M);
-                print("WERE-TO-HIT-THE-UFO-NOW----THE-FORCE-OF-THE-EXPLOSION-WOULD");
-                print("----------DESTROY-YOU-AS-WELL-AS-THE-UFO-!");
-                yes_or_no = getYesOrNo();
-                if (yes_or_no == 'N'){
+                PRINT("---------------ATTENTION----------------\n");
+                PRINT("ELAPSED-TIME-INDICATES-THAT- %d MINUTES-HAVE-PASSED.--IF-YOU-\n",M);
+                PRINT("WERE-TO-HIT-THE-UFO-NOW----THE-FORCE-OF-THE-EXPLOSION-WOULD\n");
+                PRINT("----------DESTROY-YOU-AS-WELL-AS-THE-UFO-!\n");
+                PRINT("TRY AGAIN? ");
+                char *rules_flag;
+                input_string(&rules_flag);
+                if (strcmp (rules_flag, "NO") == 0){
                   exit(0); 
                 }
                 else {
@@ -214,10 +176,12 @@ while (1)
               if (X <= H) 
               {
                 if (count1 == (int)C){
-                  print("FANTASTIC!!!!  YOU HIT THE UFO EXACTLY ON PROJECTED COURSE");
-                  print("YOU MUST BE VERY SHARP!\n");
-                  yes_or_no = getYesOrNo();
-                  if (yes_or_no == 'N'){
+                  PRINT("FANTASTIC!!!!  YOU HIT THE UFO EXACTLY ON PROJECTED COURSE\n");
+                  PRINT("YOU MUST BE VERY SHARP!\n");
+                  PRINT("TRY AGAIN? ");
+                  char *rules_flag;
+                  input_string(&rules_flag);
+                  if (strcmp (rules_flag, "NO") == 0){
                     exit(0); 
                   }
                   else {
@@ -231,23 +195,25 @@ while (1)
                   if (count1 < 200) 
                   {
                     if (count1==C-1){
-                      print("YOU ARE SHY OF THE UFO'S COORDINATES,");
-                      print("HOWEVER THIS IS ONLY A SLIGHT DEVIATION SO THE");
-                      print("UFO HAS BEEN DESTROYED!");
+                      PRINT("YOU ARE SHY OF THE UFO'S COORDINATES,\n");
+                      PRINT("HOWEVER THIS IS ONLY A SLIGHT DEVIATION SO THE\n");
+                      PRINT("UFO HAS BEEN DESTROYED!\n");
                     } 
                     else if (count1==C+1) 
                     {
-                      print("YOU OVERSHOT THE UFO'S COORDINATES,");
-                      print("HOWEVER THIS IS ONLY A SLIGHT DEVIATION SO THE");
-                      print("UFO HAS BEEN DESTROYED!");
+                      PRINT("YOU OVERSHOT THE UFO'S COORDINATES,\n");
+                      PRINT("HOWEVER THIS IS ONLY A SLIGHT DEVIATION SO THE\n");
+                      PRINT("UFO HAS BEEN DESTROYED!\n");
                     }
                   } 
                   else if (count1>=200) 
                   {
-                    print("GOOD WORK! THERE GOES THE MISSILE--------RIGHT TOWARDS");
-                    print("RUSSIA!!!! NOW YOU'RE DEFINITELY IN TROUBLE!!");
-                    yes_or_no = getYesOrNo();
-                    if (yes_or_no == 'N'){
+                    PRINT("GOOD WORK! THERE GOES THE MISSILE--------RIGHT TOWARDS\n");
+                    PRINT("RUSSIA!!!! NOW YOU'RE DEFINITELY IN TROUBLE!!\n");
+                    PRINT("TRY AGAIN? ");
+                    char *rules_flag;
+                    input_string(&rules_flag);
+                    if (strcmp (rules_flag, "NO") == 0){
                       exit(0); 
                     }
                     else {
@@ -259,12 +225,14 @@ while (1)
                 } 
                 else if (count1 <= 10) 
                 {
-                  print("AT THAT COURSE YOU SHOT YOUR MISSILE STRAIGHT UP, SO WHEN");
-                  printf("IT RUNS OUT OF FUEL IN ABOUT %.f HOURS, IT WILL FALL", F);
-                  print("THROUGH");
-                  print("YOUR ROOF!!!!!!!!!!  GOODBYE!!!!!!");
-                  yes_or_no = getYesOrNo();
-                  if (yes_or_no == 'N'){
+                  PRINT("\nAT THAT COURSE YOU SHOT YOUR MISSILE STRAIGHT UP, SO WHEN\n");
+                  PRINT("IT RUNS OUT OF FUEL IN ABOUT %.f HOURS, IT WILL FALL\n", F);
+                  PRINT("THROUGH\n");
+                  PRINT("YOUR ROOF!!!!!!!!!!  GOODBYE!!!!!!\n");
+                  PRINT("TRY AGAIN? ");
+                  char *rules_flag;
+                  input_string(&rules_flag);
+                  if (strcmp (rules_flag, "NO") == 0){
                     exit(0); 
                   }
                   else {
@@ -276,11 +244,13 @@ while (1)
               } 
               else 
               {
-                printf( "SORRY--THE SPEED OF THE UFO HAS EXCEEDED %.f M.P.H.",H);
-                print("THIS SPEED IS TO GREAT FOR YOUR DEFENSE SCREEN TO TRACK!");
-                print("THEREFORE, YOU ARE (HA HA) DOOMED!");
-                yes_or_no = getYesOrNo();
-                if (yes_or_no == 'N'){
+                PRINT( "SORRY--THE SPEED OF THE UFO HAS EXCEEDED %.f M.P.H.\n",H);
+                PRINT("THIS SPEED IS TO GREAT FOR YOUR DEFENSE SCREEN TO TRACK!\n");
+                PRINT("THEREFORE, YOU ARE (HA HA) DOOMED!\n");
+                char *rules_flag;
+                input_string(&rules_flag);
+                PRINT("TRY AGAIN? ");
+                if (strcmp (rules_flag, "NO") == 0){
                     exit(0); 
                 }
                 else {
@@ -295,13 +265,14 @@ while (1)
               C=Z-Y+5;
               T=T+0.5;
             }        
-          } else if ( buffer[0] == 'B' || buffer[0] == 'b' ) {
+          } else if ( strcmp (rules_flagY, "B") == 0  ) 
+          {
             F = 0;
             A=(randFloat()*5)+1;
             C=Z-Y+A;
             M=12000;
-            print("\n MILES   SPEED   LONGITUDE     LATITUDE    COURSE");
-            print("------   -----   ----------   ----------   ------");
+            printf("\n MILES   SPEED   LONGITUDE     LATITUDE    COURSE\n");
+            printf("------   -----   ----------   ----------   ------\n");
             while (M>0){
               P=X+1;
               printf("%d     %.f       %.f          %.f         %.f\n",M,X,Y,Z,C);
@@ -322,8 +293,10 @@ while (1)
               printf("---  HAVE JUST OBSERVED EXPLOSION AT COORDINATES (%.f,0).\n",0-F);
               A=(A+2)*A;
               printf("BLAST SEEN FROM %.f MILES AWAY.  NO SURVIVORS.\n",pow(A,3));
-              yes_or_no = getYesOrNo();
-              if (yes_or_no == 'N'){
+              PRINT("TRY AGAIN? ");
+              char *rules_flag;
+              input_string(&rules_flag);
+              if (strcmp (rules_flag, "NO") == 0){
                 exit(0);
               }  
               else {
@@ -333,17 +306,19 @@ while (1)
             } else {
               printf("GOOD WORK.  FRICTION OF %.f",F);
               printf(" %% HAS CAUSED THE COURSE\n");
-              print("OF THE UFO TO DEVIATE.  IMPACT COORDINATES ARE\n");
+              printf("OF THE UFO TO DEVIATE.  IMPACT COORDINATES ARE\n");
               printf("NOW (%.f , %.f).  YOU MADE IT!\n",F,0-F);
-              yes_or_no = getYesOrNo();    
-              if ( yes_or_no == 'N' ) {
+              PRINT("TRY AGAIN? ");
+              char *rules_flag;
+              input_string(&rules_flag);
+              if (strcmp (rules_flag, "NO") == 0){
                 exit(0); 
               } else {
                 N = N+1;
                 game_state = STATE_CHOOSE;
               }
             }
-      }
+          }
     }  else
             {
                 printf( "Internal error #2, fort %d does not exist\n", game_state );
--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
-project:
-	gcc 123.c -Wall -pedantic -o a.exe
-project : 123.c
-	gcc -c 123.c -o 123.o
+win:
+	gcc -std=c99 -Wall -Wpedantic 123.c utils.c functions.c -o gamewin.exe
+	./gamewin.exe
+linux:
+	gcc 123.c utils.c functions.c -o gamelinux 
+	./gamelinux
 installemsdk: 
-	cd .. && git clone https://github.com/emscripten-core/emsdk.git
-	cd ../emsdk && emsdk install latest && emsdk activate latest && emsdk_env.bat && emcc 123.c -s NO_EXIT_RUNTIME=0 -o output.html
-	npm i http-server -g
-runserver:
-	cd ../emsdk && http-server
+	cd ../emsdk && emsdk_env.bat && cd ../project && emcc 123.c utils.c web.c -s NO_EXIT_RUNTIME=0 -o gameweb.html -sSINGLE_FILE -sASYNCIFY
